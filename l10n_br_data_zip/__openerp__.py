79c11a6c3551398ce15de0e5b7df24393e368d0c
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2015-03-06 09:15:25 -0300

    Included module "l10n_br_data_zip" (from Brazilian Localization).

---
+++ l10n_br_data_zip/__openerp__.py
@@ -0,0 +1,54 @@
+# -*- encoding: utf-8 -*-
+###############################################################################
+#                                                                             #
+# Copyright (C) 2009  Renato Lima - Akretion                                  #
+#                                                                             #
+#This program is free software: you can redistribute it and/or modify         #
+#it under the terms of the GNU Affero General Public License as published by  #
+#the Free Software Foundation, either version 3 of the License, or            #
+#(at your option) any later version.                                          #
+#                                                                             #
+#This program is distributed in the hope that it will be useful,              #
+#but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+#GNU Affero General Public License for more details.                          #
+#                                                                             #
+#You should have received a copy of the GNU Affero General Public License     #
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
+###############################################################################
+
+{
+    'name': 'Brazilian zip codes datas',
+    'description': 'Brazilian zip codes datas',
+    'license': 'AGPL-3',
+    'author': 'Akretion, OpenERP Brasil',
+    'version': '0.1',
+    'depends': [
+        'l10n_br_zip',
+    ],
+    'data': [
+        'zip_00.sql',
+        'zip_01_01.sql',
+        'zip_01_02.sql',
+        'zip_02.sql',
+        'zip_03.sql',
+        'zip_04.sql',
+        'zip_05.sql',
+        'zip_06.sql',
+        'zip_07.sql',
+        'zip_08.sql',
+        'zip_09.sql',
+        'zip_10.sql',
+        'zip_11.sql',
+        'zip_12.sql',
+        'zip_13.sql',
+        'zip_14.sql',
+        'zip_15.sql',
+        'zip_16.sql',
+        'zip_17.sql',
+    ],
+    'demo': [],
+    'category': 'Localization',
+    'active': False,
+    'installable': True
+}
